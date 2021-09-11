7eb072f5a456b7099a232c73e39912eb9403797b
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2021-09-12 00:19:23 +0900

    Update manager.py

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -52,7 +52,11 @@ def manager_init():
     ("SccSmootherSyncGasPressed", "0"),
     ("StockNaviDecelEnabled", "0"),
     ("ShowDebugUI", "0"),
-    ("CustomLeadMark", "0")
+    ("CustomLeadMark", "0"),
+    
+    ("SccSmootherState", "0"),
+    ("SccSmootherEnabled", "0"),
+    ("SccSmootherSwitchGapOnly", "0")    
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
