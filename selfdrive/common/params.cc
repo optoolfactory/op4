e105dcb4663cf3e279ef0be16927b6ceaa6f3008
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2021-09-12 00:14:49 +0900

    Update params.cc

--- selfdrive/common/params.cc
+++ selfdrive/common/params.cc
@@ -223,6 +223,9 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"CustomLeadMark", PERSISTENT},
 
     {"SoftRestartTriggered", CLEAR_ON_MANAGER_START},
+  
+    {"SccSmootherEnabled", PERSISTENT},
+    {"SccSmootherSwitchGapOnly", PERSISTENT}  
 };
 
 } // namespace
