9e3bb78aa8a31152d9c3c54fa8c347b78861066f
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2021-09-14 22:06:29 +0900

    Update scc_smoother.py

--- selfdrive/car/hyundai/scc_smoother.py
+++ selfdrive/car/hyundai/scc_smoother.py
@@ -170,8 +170,8 @@ class SccSmoother:
 
     # kph
     apply_limit_speed, road_limit_speed, left_dist, first_started, max_speed_log = \
-      road_speed_limiter_get_max_speed(clu11_speed, self.is_metric)
-
+      road_speed_limiter_get_max_speed(clu11_speed, controls.v_cruise_kph
+                                       
     self.cal_curve_speed(sm, CS.out.vEgo, frame)
     if self.slow_on_curves and self.curve_speed_ms >= MIN_CURVE_SPEED:
       max_speed_clu = min(controls.v_cruise_kph * CV.KPH_TO_MS, self.curve_speed_ms) * self.speed_conv_to_clu
