20e3742b3f5f25d84c834ad24458905d994961d7
agegold <66872114+agegold@users.noreply.github.com>
2021-10-14 16:13:28 +0900

    Update panda.h

--- selfdrive/boardd/panda.h
+++ selfdrive/boardd/panda.h
@@ -57,6 +57,7 @@ class Panda {
   std::atomic<bool> connected = true;
   std::atomic<bool> comms_healthy = true;
   cereal::PandaState::PandaType hw_type = cereal::PandaState::PandaType::UNKNOWN;
+  bool is_pigeon = false;
   bool has_rtc = false;
 
   // Static functions
